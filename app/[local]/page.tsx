4e563b924f2abcc1c21ca3385d3775ee1ce1c7e9
damienh9723 <helo.dams@gmail.com>
2023-11-19 12:33:05 +0100

    feat: home page base

--- app/[local]/page.tsx
+++ app/[local]/page.tsx
@@ -1,5 +1,18 @@
+import { useTranslations } from 'next-intl';
+
 const Home = () => {
-  return <div>Home</div>;
+  const t = useTranslations('Index');
+  return (
+    <section className='w-full flex-center flex-col'>
+      <h1 className='head_text text-center'>
+        {t('main_title')}
+        <br className='max-md:hidden' />
+        <span className='teal_gradient text-center'>{t('sub_title')}</span>
+      </h1>
+      <p className='desc text-center'>{t('description')}</p>
+      {/* <Feed /> */}
+    </section>
+  );
 };
 
 export default Home;
